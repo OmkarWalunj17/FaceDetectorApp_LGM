1926a3c3850a45525e49204190f70f4a060ea7c9
OmkarWalunj17 <omkarwalunj03@gmail.com>
2024-03-31 23:13:32 +0530

    Updated FaceDetection.java

--- app/src/main/java/com/example/facedetector/FaceDetection.java
+++ app/src/main/java/com/example/facedetector/FaceDetection.java
@@ -4,13 +4,7 @@ import android.app.Application;
 
 import com.google.firebase.FirebaseApp;
 
-// this file is for initializing and detetcting the faces
-
 public class FaceDetection extends Application {
-
-//    public static final String RESULT_TEXT = "RESULT_TEXT";
-//    public static final String RESULT_DIALOG = "RESULT_DIALOG";
-
     @Override
     public void onCreate() {
         super.onCreate();
